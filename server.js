c429dcd0464cf48dde7e76529126378b20d58c19
israr2018 <israr.sharepoint@gmail.com>
2018-11-09 01:31:28 -0800

    testing the server.js file

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app=express();
 app.use(express.static(__dirname+'/dist/MCZA6'));
 app.get('/*',function(req,res){
 
-res.send('./dist/MCZA6/index.html');
+//res.send('./dist/MCZA6/index.html');
+res.send("hello world");
 
 
 });
