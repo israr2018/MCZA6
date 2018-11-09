c74e6311f192074ca0cf930f5267d52d9f6418cf
israr2018 <israr.sharepoint@gmail.com>
2018-11-09 01:25:48 -0800

    fixing ./dist/MCZA6/index.html

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app=express();
 app.use(express.static(__dirname+'/dist/MCZA6'));
 app.get('/*',function(req,res){
 
-res.send('./dist/index.html');
+res.send('./dist/MCZA6/index.html');
 
 
 });
