c408a2de60875cc058d71a32d31772dda82fd324
israr2018 <israr.sharepoint@gmail.com>
2018-11-09 14:36:08 -0800

    Final Changes

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ app.use(express.static(__dirname+'/dist/MCZA6'));
 const port=process.env.PORT||8080;
 app.get('/*',function(req,res){
 
-res.sendFile('./dist/MCZA6/index.html');
-
+//res.sendFile('./dist/MCZA6/index.html');
+//res.send(__dirname+'/dist/MCZA6");
+res.sendFile(path.join(__dirname+'/dist/MCZA6/index.html'));
 
 
 });
